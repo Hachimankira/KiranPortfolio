e9820bc8abcd4dc4d001bceb7d098985490d3dad
Hachimankira <kiran9860819025@gmail.com>
2024-07-30 17:42:19 +0545

    404 error

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="kiranshrestha111.com.np">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
